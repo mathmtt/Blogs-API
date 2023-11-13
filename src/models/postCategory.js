7e09db8338ae31d80cbdd09647f0649d77a60415
Matheus Motta <matheusmotta136@gmail.com>
2023-11-13 00:42:54 -0300

    tentando resolvel o pq o git hub nao esta aceitando o meu requisito 7,10 e 11

--- src/models/postCategory.js
+++ src/models/postCategory.js
@@ -1,71 +1,33 @@
 const PostCategorySchema = (sequelize, DataTypes) => {
 
   const PostCategoryTable = sequelize.define(
-
     'PostCategory',
-
     {
-
       postId: DataTypes.INTEGER,
-
       categoryId: DataTypes.INTEGER,
-
     },
-
     {
-
       timestamps: false,
-
       underscored: true,
-
       tableName: 'posts_categories',
-
     }
-
   );
 
-
-
-
   PostCategoryTable.associate = ({ BlogPost, Category }) => {
-
     BlogPost.belongsToMany(Category, {
-
       foreignKey: 'postId',
-
       otherKey: 'categoryId',
-
       as: 'categories',
-
       through: PostCategoryTable,
-
     });
-
-
-
 
     Category.belongsToMany(BlogPost, {
-
       foreignKey: 'categoryId',
-
       otherKey: 'postId',
-
       as: 'blogpost',
-
       through: PostCategoryTable,
-
     });
-
   };
-
-
-
-
   return PostCategoryTable;
-
 };
-
-
-
-
 module.exports = PostCategorySchema;
